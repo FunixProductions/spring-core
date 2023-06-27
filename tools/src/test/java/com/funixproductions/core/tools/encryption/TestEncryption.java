2e0eb24ace5a28fff3211f849142a26b7bd1a608
apronnier <apronnier@groupeastek.fr>
2023-06-27 11:25:51 +0200

    Removed encryption files, switched to encryption files

--- tools/src/test/java/com/funixproductions/core/tools/encryption/TestEncryption.java
+++ tools/src/test/java/com/funixproductions/core/tools/encryption/TestEncryption.java
@@ -1,28 +1,35 @@
 package com.funixproductions.core.tools.encryption;
 
+import com.google.common.base.Strings;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Need env configured to run this test
+ * You need to set the environment variables ENCRYPTION_KEY and ENCRYPTION_IV
+ */
 class TestEncryption {
 
-    private final Encryption encryption;
-
-    public TestEncryption() {
-        this.encryption = new Encryption() {};
-    }
-
     @Test
-    void testEncrypt() throws Exception {
+    void testEncryptAndDecryptWithEnv() {
+        final Encryption encryption = new Encryption() {};
+
         final String str = "Bonjour je suis encrypté";
         final String encoded = encryption.convertToDatabase(str);
         final String decoded = encryption.convertToEntity(encoded);
 
-        System.out.printf("String encoded : %s\n", encoded);
-        System.out.printf("String decoded : %s\n", decoded);
         assertNotEquals(str, encoded);
         assertEquals(str, decoded);
     }
 
+    @Test
+    void testFetchNewEncryptionKey() {
+        assertFalse(Strings.isNullOrEmpty(Encryption.generateNewEncryptionKey()));
+    }
+
+    @Test
+    void testFetchNewEncryptionIvKey() {
+        assertFalse(Strings.isNullOrEmpty(Encryption.generateIvEncryptionKey()));
+    }
 }
