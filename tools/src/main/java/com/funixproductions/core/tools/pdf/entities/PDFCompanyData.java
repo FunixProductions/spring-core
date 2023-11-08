f53d9fd48f4c4895f3b753ca98be85452cd94176
Antoine PRONNIER <antoine.pronnier@gmail.com>
2023-11-08 13:21:39 +0100

    wip header and footer

--- tools/src/main/java/com/funixproductions/core/tools/pdf/entities/PDFCompanyData.java
+++ tools/src/main/java/com/funixproductions/core/tools/pdf/entities/PDFCompanyData.java
@@ -1,4 +1,32 @@
 package com.funixproductions.core.tools.pdf.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class PDFCompanyData {
+
+    private String name;
+
+    private String address;
+
+    private String zipCode;
+
+    private String city;
+
+    private String phone;
+
+    private String email;
+
+    private String website;
+
+    private String siret;
+
+    private String tvaCode;
+
 }
--- tools/src/main/java/com/funixproductions/core/tools/pdf/generators/PDFGeneratorWithHeaderAndFooter.java
+++ tools/src/main/java/com/funixproductions/core/tools/pdf/generators/PDFGeneratorWithHeaderAndFooter.java
@@ -1,10 +1,12 @@
 package com.funixproductions.core.tools.pdf.generators;
 
 import com.funixproductions.core.exceptions.ApiException;
+import com.funixproductions.core.tools.pdf.entities.PDFCompanyData;
 import lombok.NonNull;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import org.springframework.lang.Nullable;
 
 import java.io.File;
 
@@ -12,6 +14,10 @@ import java.io.File;
 @Slf4j(topic = "PDFGeneratorHeaderFooter")
 public abstract class PDFGeneratorWithHeaderAndFooter extends PDFGenerator {
 
+    private static final String ERROR_SLF4J = "An error occurred while adding the header logo to the pdf {}";
+    private static final String ERROR_API = "Une erreur est survenue lors de l'ajout du logo en header sur le pdf ";
+
+    private final PDFCompanyData companyData;
     private PDImageXObject headerLogo;
 
     /**
@@ -19,34 +25,54 @@ public abstract class PDFGeneratorWithHeaderAndFooter extends PDFGenerator {
      *
      * @param pdfName the name of the PDF file
      */
-    protected PDFGeneratorWithHeaderAndFooter(@NonNull String pdfName) {
+    protected PDFGeneratorWithHeaderAndFooter(@NonNull String pdfName, @Nullable PDFCompanyData companyData) {
         super(pdfName);
+        this.companyData = companyData;
     }
 
-    @Override
-    protected void newPage() throws ApiException {
-        super.newPage();
 
-        if (this.headerLogo != null) {
-            try {
-                super.contentStream.drawImage(this.headerLogo, 50, 50);
-                super.yPosition += this.headerLogo.getHeight() + super.lineSpacing;
-            } catch (final Exception e) {
-                log.error("An error occurred while adding the header logo to the pdf {}", super.pdfName, e);
-                throw new ApiException("Une erreur est survenue lors de l'ajout du logo en header sur le pdf " + super.pdfName, e);
-            }
-        }
-    }
+    protected PDFGeneratorWithHeaderAndFooter(@NonNull String pdfName, @Nullable PDFCompanyData companyData, @NonNull File headerLogoFile) {
+        super(pdfName);
+        this.companyData = companyData;
 
-    public void setHeaderLogo(File headerLogoFile) throws ApiException {
         try {
             if (!headerLogoFile.exists()) {
                 throw new ApiException("Header logo file does not exist: " + headerLogoFile.getAbsolutePath());
             }
             this.headerLogo = PDImageXObject.createFromFile(headerLogoFile.getPath(), super.pdfDocument);
         } catch (final Exception e) {
-            log.error("An error occurred while adding the header logo to the pdf {}", super.pdfName, e);
-            throw new ApiException("Une erreur est survenue lors de l'ajout du logo en header sur le pdf " + super.pdfName, e);
+            log.error(ERROR_SLF4J, super.pdfName, e);
+            throw new ApiException(ERROR_API + super.pdfName, e);
+        }
+    }
+
+    protected PDFGeneratorWithHeaderAndFooter(@NonNull String pdfName, @Nullable PDFCompanyData companyData, byte[] imageByteArray, @NonNull String imageName) {
+        super(pdfName);
+        this.companyData = companyData;
+
+        try {
+            this.headerLogo = PDImageXObject.createFromByteArray(super.pdfDocument, imageByteArray, imageName);
+        } catch (final Exception e) {
+            log.error(ERROR_SLF4J, super.pdfName, e);
+            throw new ApiException(ERROR_API + super.pdfName, e);
+        }
+    }
+
+    @Override
+    protected void newPage() throws ApiException {
+        super.newPage();
+        setHeaderLogoOnNewPage();
+    }
+
+    private void setHeaderLogoOnNewPage() throws ApiException {
+        if (this.headerLogo != null) {
+            try {
+                super.contentStream.drawImage(this.headerLogo, 50, 50);
+                super.yPosition += this.headerLogo.getHeight() + super.lineSpacing;
+            } catch (final Exception e) {
+                log.error(ERROR_SLF4J, super.pdfName, e);
+                throw new ApiException(ERROR_API + super.pdfName, e);
+            }
         }
     }
 }
