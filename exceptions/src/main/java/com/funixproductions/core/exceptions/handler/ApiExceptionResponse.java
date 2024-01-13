4d36c68383b98321ec5f7c440d5f718fa78cdb48
Antoine PRONNIER <antoine.pronnier@gmail.com>
2024-01-13 14:39:31 +0100

    New validation exception handling

--- exceptions/src/main/java/com/funixproductions/core/exceptions/handler/ApiExceptionResponse.java
+++ exceptions/src/main/java/com/funixproductions/core/exceptions/handler/ApiExceptionResponse.java
@@ -1,11 +1,16 @@
 package com.funixproductions.core.exceptions.handler;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class ApiExceptionResponse {
     private final String error;
     private final Integer status;
+    private final long timestamp;
+
+    public ApiExceptionResponse(final String error, final Integer status) {
+        this.error = error;
+        this.status = status;
+        this.timestamp = System.currentTimeMillis();
+    }
 }
--- exceptions/src/main/java/com/funixproductions/core/exceptions/handler/ApiExceptionsHandler.java
+++ exceptions/src/main/java/com/funixproductions/core/exceptions/handler/ApiExceptionsHandler.java
@@ -4,47 +4,72 @@ import com.funixproductions.core.exceptions.ApiBadRequestException;
 import com.funixproductions.core.exceptions.ApiException;
 import com.funixproductions.core.exceptions.ApiForbiddenException;
 import com.funixproductions.core.exceptions.ApiNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+
+@Slf4j
 @RestControllerAdvice
 public class ApiExceptionsHandler {
 
     @ExceptionHandler(ApiException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ApiExceptionResponse handleBase(ApiException e) {
-        return new ApiExceptionResponse(
-                e.getMessage(),
-                HttpStatus.INTERNAL_SERVER_ERROR.value()
-        );
+        log.error("Api Exception ", e);
+        return handleException(e, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(ApiBadRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiExceptionResponse handleBadRequest(ApiBadRequestException e) {
-        return new ApiExceptionResponse(
-                e.getMessage(),
-                HttpStatus.BAD_REQUEST.value()
-        );
+        log.debug("Api Bad Request Exception ", e);
+        return handleException(e, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ApiForbiddenException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ApiExceptionResponse handleForbidden(ApiForbiddenException e) {
-        return new ApiExceptionResponse(
-                e.getMessage(),
-                HttpStatus.FORBIDDEN.value()
-        );
+        log.debug("Api Forbidden Exception ", e);
+        return handleException(e, HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(ApiNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiExceptionResponse handleNotFound(ApiNotFoundException e) {
+        log.debug("Api Not Found Exception ", e);
+        return handleException(e, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ApiExceptionResponse handleValidationException(MethodArgumentNotValidException ex) {
+        final BindingResult bindingResult = ex.getBindingResult();
+        final List<String> errorMessages = bindingResult.getFieldErrors()
+                .stream()
+                .map(this::buildErrorMessage)
+                .toList();
+        final String errorMessage = String.join(", ", errorMessages);
+
+        return new ApiExceptionResponse(
+                "Erreur requête : " + errorMessage,
+                HttpStatus.BAD_REQUEST.value()
+        );
+    }
+
+    private String buildErrorMessage(FieldError fieldError) {
+        return fieldError.getField() + " " + fieldError.getDefaultMessage();
+    }
+
+    private ApiExceptionResponse handleException(Exception e, HttpStatus status) {
         return new ApiExceptionResponse(
                 e.getMessage(),
-                HttpStatus.NOT_FOUND.value()
+                status.value()
         );
     }
 
