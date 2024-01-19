ad669d0fb7eb87d07113ea2f32ff425463ed8772
Antoine PRONNIER <antoine.pronnier@gmail.com>
2024-01-19 16:12:54 +0100

    vat info

--- tools/src/main/java/com/funixproductions/core/tools/pdf/tools/VATInformation.java
+++ tools/src/main/java/com/funixproductions/core/tools/pdf/tools/VATInformation.java
@@ -4,46 +4,47 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 /**
- * VAT information on european countries.
- * 0 for non european countries.
+ * VAT information on countries.
+ * 0 for non vat countries.
  */
 @Getter
 @RequiredArgsConstructor
 public enum VATInformation {
-    AUSTRALIA(10.0),
-    AUSTRIA(20.0),
-    BELGIUM(21.0),
-    BULGARIA(20.0),
-    CROATIA(25.0),
-    CYPRUS(19.0),
-    CZECH_REPUBLIC(21.0),
-    DENMARK(25.0),
-    ESTONIA(20.0),
-    FINLAND(24.0),
-    FRANCE(20.0),
-    GERMANY(19.0),
-    GREECE(24.0),
-    HUNGARY(27.0),
-    IRELAND(23.0),
-    ITALY(22.0),
-    LATVIA(21.0),
-    LITHUANIA(21.0),
-    LUXEMBOURG(16.0),
-    MALTA(18.0),
-    NETHERLANDS(21.0),
-    NORWAY(25.0),
-    POLAND(23.0),
-    PORTUGAL(23.0),
-    ROMANIA(19.0),
-    SINGAPORE(8.0),
-    SLOVAKIA(20.0),
-    SLOVENIA(22.0),
-    SOUTH_AFRICA(15.0),
-    SPAIN(21.0),
-    SWEDEN(25.0),
-    SWITZERLAND(7.7),
-    UNITED_KINGDOM(20.0);
+    AUSTRALIA(10.0, "AU"),
+    AUSTRIA(20.0, "AT"),
+    BELGIUM(21.0, "BE"),
+    BULGARIA(20.0, "BG"),
+    CROATIA(25.0, "HR"),
+    CYPRUS(19.0, "CY"),
+    CZECH_REPUBLIC(21.0, "CZ"),
+    DENMARK(25.0, "DK"),
+    ESTONIA(22.0, "EE"),
+    FINLAND(24.0, "FI"),
+    FRANCE(20.0, "FR"),
+    GERMANY(19.0, "DE"),
+    GREECE(24.0, "GR"),
+    HUNGARY(27.0, "HU"),
+    IRELAND(23.0, "IE"),
+    ITALY(22.0, "IT"),
+    LATVIA(21.0, "LV"),
+    LITHUANIA(21.0, "LT"),
+    LUXEMBOURG(17.0, "LU"),
+    MALTA(18.0, "MT"),
+    NETHERLANDS(21.0, "NL"),
+    NORWAY(25.0, "NO"),
+    POLAND(23.0, "PL"),
+    PORTUGAL(23.0, "PT"),
+    ROMANIA(19.0, "RO"),
+    SINGAPORE(9.0, "SG"),
+    SLOVAKIA(20.0, "SK"),
+    SLOVENIA(22.0, "SI"),
+    SOUTH_AFRICA(15.0, "ZA"),
+    SPAIN(21.0, "ES"),
+    SWEDEN(25.0, "SE"),
+    SWITZERLAND(8.1, "CH"),
+    UNITED_KINGDOM(20.0, "GB");
 
     private final double vatRate;
+    private final String countryCode;
 
 }
